dfd8959bc8c6cf030112bd84d6a49ae36528049d
Frede-72 <frjo0005@stud.ek.dk>
2026-09-04 15:27:44 +0200

    ADDED: .env.example

---
+++ 01_Python_DotEnv/src/main.py
@@ -0,0 +1,11 @@
+# from dotenv import load_dotenv
+# import os
+
+
+# load_dotenv()
+# print(os.getenv("GREETING"));
+
+from dotenv import dotenv_values
+
+config = dotenv_values(".env")
+print(config["GREETING"])
